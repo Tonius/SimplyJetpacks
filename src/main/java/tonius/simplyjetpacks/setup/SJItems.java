e222432d337c56418f2078b6e1ce6a05d3431f72
Tonius <tonbro96@gmail.com>
2014-07-24 22:44:55 +0200

    Fixed things to work outside of dev and readded TE support

--- src/main/java/tonius/simplyjetpacks/setup/SJItems.java
+++ src/main/java/tonius/simplyjetpacks/setup/SJItems.java
@@ -3,11 +3,15 @@ package tonius.simplyjetpacks.setup;
 import java.util.HashMap;
 import java.util.Map;
 
-import net.minecraft.item.Item;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import tonius.simplyjetpacks.SimplyJetpacks;
+import tonius.simplyjetpacks.config.SJConfig;
 import tonius.simplyjetpacks.integration.TEItems;
+import tonius.simplyjetpacks.integration.TERecipes;
 import tonius.simplyjetpacks.item.ItemJetpack;
 import tonius.simplyjetpacks.item.ItemMeta;
 import tonius.simplyjetpacks.item.MetaItem;
@@ -18,9 +22,9 @@ import cpw.mods.fml.common.registry.GameRegistry;
 
 public class SJItems {
 
-    public static Item jetpacks = null;
-    public static Item components = null;
-    public static Item particleCustomizers = null;
+    public static ItemJetpack jetpacks = null;
+    public static ItemMeta components = null;
+    public static ItemMeta particleCustomizers = null;
 
     private static boolean teAvailable = false;
 
@@ -42,10 +46,12 @@ public class SJItems {
         jetpacks = new ItemJetpack();
 
         Map<Integer, MetaItem> componentsMap = new HashMap<Integer, MetaItem>();
+
         String[] leatherStrapTooltips = new String[2];
         leatherStrapTooltips[0] = "tooltip.leatherStrap.description.1";
         leatherStrapTooltips[1] = "tooltip.leatherStrap.description.2";
         componentsMap.put(0, new MetaItem("leatherStrap", null, leatherStrapTooltips));
+
         String[] thrusterTooltips = new String[2];
         thrusterTooltips[0] = "tooltip.thruster.description.1";
         thrusterTooltips[1] = "tooltip.thruster.description.2";
@@ -53,6 +59,7 @@ public class SJItems {
         componentsMap.put(12, new MetaItem("thruster.2", null, thrusterTooltips));
         componentsMap.put(13, new MetaItem("thruster.3", StringUtils.YELLOW, thrusterTooltips));
         componentsMap.put(14, new MetaItem("thruster.4", StringUtils.BRIGHT_BLUE, thrusterTooltips));
+
         String[] armorPlatingTooltips = new String[2];
         armorPlatingTooltips[0] = "tooltip.armorPlating.description.1";
         armorPlatingTooltips[1] = "tooltip.armorPlating.description.2";
@@ -60,6 +67,7 @@ public class SJItems {
         componentsMap.put(122, new MetaItem("armorPlating.2", null, armorPlatingTooltips));
         componentsMap.put(123, new MetaItem("armorPlating.3", null, armorPlatingTooltips));
         componentsMap.put(124, new MetaItem("armorPlating.4", StringUtils.BRIGHT_BLUE, armorPlatingTooltips));
+
         components = new ItemMeta(componentsMap, "components");
 
         Map<Integer, MetaItem> particlesMap = new HashMap<Integer, MetaItem>();
@@ -84,114 +92,32 @@ public class SJItems {
     private static void registerRecipes() {
         SimplyJetpacks.logger.info("Registering recipes");
 
-        // wait for TE
-        // if (teAvailable) {
-        // if (MainConfig.enableCraftingJetpackTier0) {
-        // GameRegistry.addRecipe(new
-        // ShapedOreRecipe(jetpackTier0.getChargedItem(jetpackTier0).copy(), new
-        // Object[] { "S S", "NPN", "R R", 'S', Items.string, 'N', "nuggetTin",
-        // 'P', TEItems.capacitorPotato, 'R', "dustRedstone" }));
-        // }
-        //
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components,
-        // 1, 0), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotLead", 'P',
-        // TEItems.conduitItemOpaque, 'C', TEItems.powerCoilGold, 'G',
-        // "gearCopper", 'D', TEItems.dynamoSteam, 'S', TEItems.pneumaticServo
-        // }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components,
-        // 1, 1), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotInvar", 'P',
-        // TEItems.conduitItemFastOpaque, 'C', TEItems.powerCoilGold, 'G',
-        // "gearBronze", 'D', TEItems.dynamoCompression, 'S',
-        // TEItems.pneumaticServo }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components,
-        // 1, 2), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotElectrum", 'P',
-        // TEItems.conduitEnergyReinforcedEmpty, 'C', TEItems.powerCoilGold,
-        // 'G', "gearInvar", 'D', TEItems.dynamoReactant, 'S',
-        // TEItems.pneumaticServo }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components,
-        // 1, 3), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotEnderium", 'P',
-        // TEItems.conduitEnergyReinforced, 'C', TEItems.powerCoilGold, 'G',
-        // "gearElectrum", 'D', TEItems.dynamoMagmatic, 'S',
-        // TEItems.pneumaticServo }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components,
-        // 1, 4), new Object[] { "LIL", "LIL", 'L', Items.leather, 'I',
-        // "ingotIron" }));
-        //
-        // if (MainConfig.enableCraftingArmorPlating) {
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components,
-        // 1, 5), new Object[] { "TIT", "III", "TIT", 'I', "ingotIron", 'T',
-        // "ingotTin" }));
-        // }
-        //
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(jetpackTier1), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotLead", 'B', TEItems.capacitorBasic, 'T', new
-        // ItemStack(components, 1, 0), 'J', new ItemStack(components, 1, 4)
-        // }));
-        // GameRegistry.addRecipe(new JetpackUpgradingRecipe(new
-        // ItemStack(jetpackTier2), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotInvar", 'B', TEItems.capacitorHardened, 'T', new
-        // ItemStack(components, 1, 1), 'J', new ItemStack(jetpackTier1, 1,
-        // OreDictionary.WILDCARD_VALUE) }));
-        // GameRegistry.addRecipe(new JetpackUpgradingRecipe(new
-        // ItemStack(jetpackTier3), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotElectrum", 'B', TEItems.capacitorReinforced, 'T', new
-        // ItemStack(components, 1, 2), 'J', new ItemStack(jetpackTier2, 1,
-        // OreDictionary.WILDCARD_VALUE) }));
-        // GameRegistry.addRecipe(new JetpackUpgradingRecipe(new
-        // ItemStack(jetpackTier4), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotEnderium", 'B', TEItems.capacitorResonant, 'T', new
-        // ItemStack(components, 1, 3), 'J', new ItemStack(jetpackTier3, 1,
-        // OreDictionary.WILDCARD_VALUE) }));
-        //
-        // if (!MainConfig.upgradingRecipesOnly) {
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(jetpackTier2), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotInvar", 'B', TEItems.capacitorHardened, 'T', new
-        // ItemStack(components, 1, 1), 'J', new ItemStack(components, 1, 4)
-        // }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(jetpackTier3), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotElectrum", 'B', TEItems.capacitorReinforced, 'T', new
-        // ItemStack(components, 1, 2), 'J', new ItemStack(components, 1, 4)
-        // }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(jetpackTier4), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotEnderium", 'B', TEItems.capacitorResonant, 'T', new
-        // ItemStack(components, 1, 3), 'J', new ItemStack(components, 1, 4)
-        // }));
-        //
-        // GameRegistry.addRecipe(new JetpackUpgradingRecipe(new
-        // ItemStack(jetpackTier3), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotElectrum", 'B', TEItems.capacitorReinforced, 'T', new
-        // ItemStack(components, 1, 2), 'J', new ItemStack(jetpackTier1, 1,
-        // OreDictionary.WILDCARD_VALUE) }));
-        // GameRegistry.addRecipe(new JetpackUpgradingRecipe(new
-        // ItemStack(jetpackTier4), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotEnderium", 'B', TEItems.capacitorResonant, 'T', new
-        // ItemStack(components, 1, 3), 'J', new ItemStack(jetpackTier1, 1,
-        // OreDictionary.WILDCARD_VALUE) }));
-        // GameRegistry.addRecipe(new JetpackUpgradingRecipe(new
-        // ItemStack(jetpackTier4), new Object[] { "IBI", "IJI", "T T", 'I',
-        // "ingotEnderium", 'B', TEItems.capacitorResonant, 'T', new
-        // ItemStack(components, 1, 3), 'J', new ItemStack(jetpackTier2, 1,
-        // OreDictionary.WILDCARD_VALUE) }));
-        // }
-        //
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(particleCustomizers, 1, 0), new Object[] { " D ", "DCD",
-        // " D ", 'C', "dustCoal", 'D', Blocks.torch }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(particleCustomizers, 1, 1), new Object[] { " D ", "DCD",
-        // " D ", 'C', "dustCoal", 'D', Blocks.glass }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(particleCustomizers, 1, 2), new Object[] { " C ", "CCC",
-        // " C ", 'C', "dustCoal" }));
-        // GameRegistry.addRecipe(new ShapedOreRecipe(new
-        // ItemStack(particleCustomizers, 1, 3), new Object[] { " R ", " C ",
-        // "G B", 'C', "dustCoal", 'R', "dyeRed", 'G', "dyeLime", 'B', "dyeBlue"
-        // }));
-        // }
+        if (teAvailable) {
+            if (SJConfig.enableCraftingPotatoJetpack) {
+                GameRegistry.addRecipe(new ShapedOreRecipe(jetpacks.getChargedItem(jetpacks, 0).copy(), new Object[] { "S S", "NPN", "R R", 'S', Items.string, 'N', "nuggetTin", 'P', TEItems.capacitorPotato.copy(), 'R', "dustRedstone" }));
+            }
+
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components, 1, 0), new Object[] { "LIL", "LIL", 'L', Items.leather, 'I', "ingotIron" }));
+
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components, 1, 11), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotLead", 'P', Blocks.glass, 'C', TEItems.powerCoilGold.copy(), 'G', "gearCopper", 'D', TEItems.dynamoSteam.copy(), 'S', TEItems.pneumaticServo.copy() }));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components, 1, 12), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotInvar", 'P', Blocks.redstone_block, 'C', TEItems.powerCoilGold.copy(), 'G', "gearBronze", 'D', TEItems.dynamoReactant.copy(), 'S', TEItems.pneumaticServo.copy() }));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components, 1, 13), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotElectrum", 'P', "ingotSignalum", 'C', TEItems.powerCoilGold.copy(), 'G', "gearInvar", 'D', TEItems.dynamoMagmatic.copy(), 'S', TEItems.pneumaticServo.copy() }));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components, 1, 14), new Object[] { "ICI", "PGP", "DSD", 'I', "ingotEnderium", 'P', "ingotLumium", 'C', TEItems.powerCoilGold.copy(), 'G', "gearElectrum", 'D', TEItems.dynamoEnervation.copy(), 'S', TEItems.pneumaticServo.copy() }));
+
+            if (SJConfig.enableCraftingArmorPlating) {
+                GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(components, 1, 121), new Object[] { "TIT", "III", "TIT", 'I', "ingotIron", 'T', "ingotTin" }));
+            }
+
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(jetpacks, 1, 1), new Object[] { "IBI", "IJI", "T T", 'I', "ingotLead", 'B', TEItems.capacitorBasic.copy(), 'T', new ItemStack(components, 1, 11), 'J', new ItemStack(components, 1, 0) }));
+            GameRegistry.addRecipe(new JetpackUpgradingRecipe(new ItemStack(jetpacks, 1, 2), new Object[] { "IBI", "IJI", "T T", 'I', "ingotInvar", 'B', TEItems.capacitorHardened.copy(), 'T', new ItemStack(components, 1, 12), 'J', new ItemStack(jetpacks, 1, 1) }));
+            GameRegistry.addRecipe(new JetpackUpgradingRecipe(new ItemStack(jetpacks, 1, 3), new Object[] { "IBI", "IJI", "T T", 'I', "ingotElectrum", 'B', TEItems.capacitorReinforced.copy(), 'T', new ItemStack(components, 1, 13), 'J', new ItemStack(jetpacks, 1, 2) }));
+            GameRegistry.addRecipe(new JetpackUpgradingRecipe(new ItemStack(jetpacks, 1, 4), new Object[] { "IBI", "IJI", "T T", 'I', "ingotEnderium", 'B', TEItems.capacitorResonant.copy(), 'T', new ItemStack(components, 1, 14), 'J', new ItemStack(jetpacks, 1, 3) }));
+
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(particleCustomizers, 1, 0), new Object[] { " D ", "DCD", " D ", 'C', "dustCoal", 'D', Blocks.torch }));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(particleCustomizers, 1, 1), new Object[] { " D ", "DCD", " D ", 'C', "dustCoal", 'D', Blocks.glass }));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(particleCustomizers, 1, 2), new Object[] { " C ", "CCC", " C ", 'C', "dustCoal" }));
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(particleCustomizers, 1, 3), new Object[] { " R ", " C ", "G B", 'C', "dustCoal", 'R', "dyeRed", 'G', "dyeLime", 'B', "dyeBlue" }));
+        }
         for (int i = 0; i <= Jetpack.getHighestMeta(); i++) {
             Jetpack jetpack = Jetpack.getJetpack(i);
             if (jetpack != null) {
@@ -203,35 +129,30 @@ public class SJItems {
     private static void doIMC() {
         SimplyJetpacks.logger.info("Doing intermod communication");
 
-        // wait for TE
-        // if (MainConfig.enableCraftingArmorPlating) {
-        // ItemStack ingotBronze;
-        // for (int i = 0; i < OreDictionary.getOres("ingotBronze").size(); i++)
-        // {
-        // ingotBronze = OreDictionary.getOres("ingotBronze").get(i).copy();
-        // ingotBronze.stackSize = 10;
-        // TERecipes.addSmelterRecipe(3200, new ItemStack(components, 1, 5),
-        // ingotBronze, new ItemStack(components, 1, 6), null, 0);
-        // }
-        //
-        // ItemStack ingotInvar;
-        // for (int i = 0; i < OreDictionary.getOres("ingotInvar").size(); i++)
-        // {
-        // ingotInvar = OreDictionary.getOres("ingotInvar").get(i).copy();
-        // ingotInvar.stackSize = 10;
-        // TERecipes.addSmelterRecipe(4800, new ItemStack(components, 1, 6),
-        // ingotInvar, new ItemStack(components, 1, 7), null, 0);
-        // }
-        //
-        // ItemStack ingotEnderium;
-        // for (int i = 0; i < OreDictionary.getOres("ingotEnderium").size();
-        // i++) {
-        // ingotEnderium = OreDictionary.getOres("ingotEnderium").get(i).copy();
-        // ingotEnderium.stackSize = 10;
-        // TERecipes.addSmelterRecipe(6400, new ItemStack(components, 1, 7),
-        // ingotEnderium, new ItemStack(components, 1, 8), null, 0);
-        // }
-        // }
+        if (teAvailable) {
+            if (SJConfig.enableCraftingArmorPlating) {
+                ItemStack ingotBronze;
+                for (int i = 0; i < OreDictionary.getOres("ingotBronze").size(); i++) {
+                    ingotBronze = OreDictionary.getOres("ingotBronze").get(i).copy();
+                    ingotBronze.stackSize = 10;
+                    TERecipes.addSmelterRecipe(3200, new ItemStack(components, 1, 121), ingotBronze, new ItemStack(components, 1, 122), null, 0);
+                }
+
+                ItemStack ingotInvar;
+                for (int i = 0; i < OreDictionary.getOres("ingotInvar").size(); i++) {
+                    ingotInvar = OreDictionary.getOres("ingotInvar").get(i).copy();
+                    ingotInvar.stackSize = 10;
+                    TERecipes.addSmelterRecipe(4800, new ItemStack(components, 1, 122), ingotInvar, new ItemStack(components, 1, 123), null, 0);
+                }
+
+                ItemStack ingotEnderium;
+                for (int i = 0; i < OreDictionary.getOres("ingotEnderium").size(); i++) {
+                    ingotEnderium = OreDictionary.getOres("ingotEnderium").get(i).copy();
+                    ingotEnderium.stackSize = 10;
+                    TERecipes.addSmelterRecipe(6400, new ItemStack(components, 1, 123), ingotEnderium, new ItemStack(components, 1, 124), null, 0);
+                }
+            }
+        }
     }
 
 }
