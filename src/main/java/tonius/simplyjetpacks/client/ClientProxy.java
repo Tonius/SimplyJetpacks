bc2660cebb956b10381a456e841970dce6d5491c
Tonius <tonbro96@gmail.com>
2014-09-09 10:15:31 +0200

    Jetpack particles respect particles setting

--- src/main/java/tonius/simplyjetpacks/client/ClientProxy.java
+++ src/main/java/tonius/simplyjetpacks/client/ClientProxy.java
@@ -33,33 +33,37 @@ public class ClientProxy extends CommonProxy {
     
     @Override
     public void showJetpackParticles(World world, EntityLivingBase wearer, JetpackParticleType particle) {
-        Vec3 userPos = Vec3.createVectorHelper(wearer.posX, wearer.posY, wearer.posZ);
-        
-        if (!wearer.equals(mc.thePlayer)) {
-            userPos = userPos.addVector(0, 1.6D, 0);
+        if (mc.gameSettings.particleSetting == 2) {
+            return;
+        } else if (mc.gameSettings.particleSetting == 0 || mc.gameSettings.particleSetting == 1 && mc.theWorld.getTotalWorldTime() % 4L == 0) {
+            Vec3 userPos = Vec3.createVectorHelper(wearer.posX, wearer.posY, wearer.posZ);
+            
+            if (!wearer.equals(mc.thePlayer)) {
+                userPos = userPos.addVector(0, 1.6D, 0);
+            }
+            
+            Vec3 vLeft = Vec3.createVectorHelper(-0.28D, -0.95D, -0.38D);
+            vLeft.rotateAroundY(-wearer.renderYawOffset * (float) Math.PI / 180F);
+            
+            Vec3 vRight = Vec3.createVectorHelper(0.28D, -0.95D, -0.38D);
+            vRight.rotateAroundY(-wearer.renderYawOffset * (float) Math.PI / 180F);
+            
+            Vec3 vCenter = Vec3.createVectorHelper((rand.nextFloat() - 0.5F) * 0.25F, -0.95D, -0.38D);
+            vCenter.rotateAroundY(-wearer.renderYawOffset * (float) Math.PI / 180F);
+            
+            vLeft = vLeft.addVector(-wearer.motionX * 0.2D, -wearer.motionY * 0.2D, -wearer.motionZ * 0.2D);
+            vRight = vRight.addVector(-wearer.motionX * 0.2D, -wearer.motionY * 0.2D, -wearer.motionZ * 0.2D);
+            vCenter = vCenter.addVector(-wearer.motionX * 0.2D, -wearer.motionY * 0.2D, -wearer.motionZ * 0.2D);
+            
+            Vec3 v = userPos.addVector(vLeft.xCoord, vLeft.yCoord, vLeft.zCoord);
+            ParticleUtils.spawnParticle(particle, world, v.xCoord, v.yCoord, v.zCoord, 0, -0.2, 0);
+            
+            v = userPos.addVector(vRight.xCoord, vRight.yCoord, vRight.zCoord);
+            ParticleUtils.spawnParticle(particle, world, v.xCoord, v.yCoord, v.zCoord, 0, -0.2, 0);
+            
+            v = userPos.addVector(vCenter.xCoord, vCenter.yCoord, vCenter.zCoord);
+            ParticleUtils.spawnParticle(particle, world, v.xCoord, v.yCoord, v.zCoord, 0, -0.2, 0);
         }
-        
-        Vec3 vLeft = Vec3.createVectorHelper(-0.28D, -0.95D, -0.38D);
-        vLeft.rotateAroundY(-wearer.renderYawOffset * (float) Math.PI / 180F);
-        
-        Vec3 vRight = Vec3.createVectorHelper(0.28D, -0.95D, -0.38D);
-        vRight.rotateAroundY(-wearer.renderYawOffset * (float) Math.PI / 180F);
-        
-        Vec3 vCenter = Vec3.createVectorHelper((rand.nextFloat() - 0.5F) * 0.25F, -0.95D, -0.38D);
-        vCenter.rotateAroundY(-wearer.renderYawOffset * (float) Math.PI / 180F);
-        
-        vLeft = vLeft.addVector(-wearer.motionX * 0.2D, -wearer.motionY * 0.2D, -wearer.motionZ * 0.2D);
-        vRight = vRight.addVector(-wearer.motionX * 0.2D, -wearer.motionY * 0.2D, -wearer.motionZ * 0.2D);
-        vCenter = vCenter.addVector(-wearer.motionX * 0.2D, -wearer.motionY * 0.2D, -wearer.motionZ * 0.2D);
-        
-        Vec3 v = userPos.addVector(vLeft.xCoord, vLeft.yCoord, vLeft.zCoord);
-        ParticleUtils.spawnParticle(particle, world, v.xCoord, v.yCoord, v.zCoord, 0, -0.2, 0);
-        
-        v = userPos.addVector(vRight.xCoord, vRight.yCoord, vRight.zCoord);
-        ParticleUtils.spawnParticle(particle, world, v.xCoord, v.yCoord, v.zCoord, 0, -0.2, 0);
-        
-        v = userPos.addVector(vCenter.xCoord, vCenter.yCoord, vCenter.zCoord);
-        ParticleUtils.spawnParticle(particle, world, v.xCoord, v.yCoord, v.zCoord, 0, -0.2, 0);
     }
     
     @Override
