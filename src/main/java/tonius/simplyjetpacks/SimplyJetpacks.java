68953f3b0b113d69fe8d3ad7442abf53382d22ce
Tonius <tonbro96@gmail.com>
2015-05-04 14:50:34 +0200

    Added version in @Mod and replaced CoFHLib crash with a custom FML error screen

--- src/main/java/tonius/simplyjetpacks/SimplyJetpacks.java
+++ src/main/java/tonius/simplyjetpacks/SimplyJetpacks.java
@@ -2,6 +2,8 @@ package tonius.simplyjetpacks;
 
 import java.util.Iterator;
 
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiErrorScreen;
 import net.minecraftforge.oredict.RecipeSorter;
 import net.minecraftforge.oredict.RecipeSorter.Category;
 
@@ -13,6 +15,7 @@ import tonius.simplyjetpacks.handler.SyncHandler;
 import tonius.simplyjetpacks.network.PacketHandler;
 import tonius.simplyjetpacks.setup.ModItems;
 import tonius.simplyjetpacks.setup.Packs;
+import cpw.mods.fml.client.CustomModLoadingErrorDisplayException;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -23,13 +26,14 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStoppingEvent;
 
-@Mod(modid = SimplyJetpacks.MODID, dependencies = SimplyJetpacks.DEPENDENCIES, guiFactory = SimplyJetpacks.GUI_FACTORY)
+@Mod(modid = SimplyJetpacks.MODID, version = SimplyJetpacks.VERSION, dependencies = SimplyJetpacks.DEPENDENCIES, guiFactory = SimplyJetpacks.GUI_FACTORY)
 public class SimplyJetpacks {
     
     public static final String MODID = "simplyjetpacks";
+    public static final String VERSION = "1.5.0";
     public static final String PREFIX = MODID + ".";
     public static final String RESOURCE_PREFIX = MODID + ":";
-    public static final String DEPENDENCIES = "after:CoFHCore;after:ThermalExpansion;after:RedstoneArsenal;after:EnderIO;after:BuildCraft|Core";
+    public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);after:CoFHCore;after:ThermalExpansion;after:RedstoneArsenal;after:EnderIO;after:BuildCraft|Core";
     public static final String GUI_FACTORY = "tonius.simplyjetpacks.config.ConfigGuiFactory";
     
     @Instance(MODID)
@@ -44,11 +48,7 @@ public class SimplyJetpacks {
         logger = evt.getModLog();
         logger.info("Starting Simply Jetpacks");
         
-        if (!isCoFHLibLoaded()) {
-            logger.error("Could not find CoFHLib!");
-            throw new RuntimeException("Could not find CoFHLib. Please install the latest version of CoFH Core, or the latest build of CoFHLib.");
-        }
-        logger.info("Successfully found CoFHLib");
+        checkCoFHLib();
         
         Packs.preInit();
         Config.preInit(evt);
@@ -68,15 +68,32 @@ public class SimplyJetpacks {
         SyncHandler.clearAll();
     }
     
-    private static boolean isCoFHLibLoaded() {
+    private static void checkCoFHLib() {
         Iterable<? extends ModContainer> list = ModAPIManager.INSTANCE.getAPIList();
         for (Iterator<? extends ModContainer> itr = list.iterator(); itr.hasNext();) {
             ModContainer api = itr.next();
             if (api.getName().equals("API: CoFHLib")) {
-                return true;
+                logger.info("Successfully found CoFHLib");
+                return;
             }
         }
-        return false;
+        logger.error("Could not find CoFHLib!");
+        throw new CustomModLoadingErrorDisplayException() {
+            
+            @Override
+            public void initGui(GuiErrorScreen errorScreen, FontRenderer fontRenderer) {
+            }
+            
+            @Override
+            public void drawScreen(GuiErrorScreen gui, FontRenderer fontRenderer, int mouseRelX, int mouseRelY, float tickTime) {
+                gui.drawDefaultBackground();
+                gui.drawCenteredString(fontRenderer, "Simply Jetpacks Error - CoFHLib Not Found", gui.width / 2, 85, 0xFF5555);
+                
+                gui.drawCenteredString(fontRenderer, "CoFHLib is not installed. Please install the latest version of CoFH Core,", gui.width / 2, 100, 0xFFFFFF);
+                gui.drawCenteredString(fontRenderer, "or the latest build of standalone CoFHLib.", gui.width / 2, 110, 0xFFFFFF);
+            }
+            
+        };
     }
     
 }
