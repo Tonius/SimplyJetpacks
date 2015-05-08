3ea2d1fca3e56ccf3e9e5ce92c0070d4dc2431ff
Tonius <tonbro96@gmail.com>
2015-05-08 14:58:55 +0200

    Replaced direct calls to BC API with IMC messages

--- src/main/java/tonius/simplyjetpacks/integration/BCRecipes.java
+++ src/main/java/tonius/simplyjetpacks/integration/BCRecipes.java
@@ -1,14 +1,27 @@
 package tonius.simplyjetpacks.integration;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import tonius.simplyjetpacks.SimplyJetpacks;
-import buildcraft.api.recipes.BuildcraftRecipeRegistry;
+import cpw.mods.fml.common.event.FMLInterModComms;
 
 public abstract class BCRecipes {
     
     public static void addAssemblyRecipe(String recipeId, int energy, ItemStack[] inputs, ItemStack output) {
         SimplyJetpacks.logger.info("Registering BC Assembly Table recipe");
         
-        BuildcraftRecipeRegistry.assemblyTable.addRecipe("simplyjetpacks:" + recipeId, energy, output, (Object[]) inputs);
+        NBTTagCompound toSend = new NBTTagCompound();
+        toSend.setString("id", "simplyjetpacks:" + recipeId);
+        toSend.setInteger("energy", energy);
+        NBTTagList inputsList = new NBTTagList();
+        for (ItemStack stack : inputs) {
+            inputsList.appendTag(stack.writeToNBT(new NBTTagCompound()));
+        }
+        toSend.setTag("input", inputsList);
+        toSend.setTag("output", output.writeToNBT(new NBTTagCompound()));
+        
+        FMLInterModComms.sendMessage("BuildCraft|Core", "add-assembly-recipe", toSend);
     }
+    
 }
