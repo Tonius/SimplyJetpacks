82d3f262fc9d97ff81f8697ad7258ea40c235830
Tonius <tonbro96@gmail.com>
2015-05-16 15:39:36 +0200

    Added specific version requirements for EnderIO and BC6/7

--- src/main/java/tonius/simplyjetpacks/SimplyJetpacks.java
+++ src/main/java/tonius/simplyjetpacks/SimplyJetpacks.java
@@ -26,7 +26,7 @@ public class SimplyJetpacks {
     public static final String VERSION = "1.5.0";
     public static final String PREFIX = MODID + ".";
     public static final String RESOURCE_PREFIX = MODID + ":";
-    public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);after:ThermalExpansion;after:RedstoneArsenal;after:EnderIO;after:BuildCraft|Core";
+    public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);after:ThermalExpansion;after:RedstoneArsenal;after:RArm;after:EnderIO@[1.7.10-2.1.3.243,);after:BuildCraft|Core@[6.4.15,7.0.0),[7.0.4,)";
     public static final String GUI_FACTORY = "tonius.simplyjetpacks.config.ConfigGuiFactory";
     
     @Instance(MODID)
